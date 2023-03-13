835632e33814b55c49a1acda06bf61f3cb1b7dc7
Benjamin Hall <benjamin1995hall@gmail.com>
2023-03-13 16:47:10 +0000

    First intrument pal push

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name, presence: true, uniqueness: true
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
