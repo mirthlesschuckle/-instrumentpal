138687476caefc178c6a27a19588151fb6168c1b
Jianhao Yu <yujianhao1314@gmail.com>
2023-03-14 15:04:04 +0000

    Created show, new and create mothods

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -2,4 +2,25 @@ class InstrumentsController < ApplicationController
   def index
     @instruments = Instrument.all
   end
+
+  def show
+    @instrument = Instrument.find(params[:id])
+  end
+
+  def new
+    @instrument = Instrument.new
+  end
+
+  def create
+    @instrument = Instrument.new(instrument_params)
+    @instrument.user = current_user
+    @instrument.save
+    redirect_to instruments_path(@instrument)
+  end
+
+  private
+
+  def instrument_params
+    params.require(:instrument).permit(:location, :model, :price)
+  end
 end
