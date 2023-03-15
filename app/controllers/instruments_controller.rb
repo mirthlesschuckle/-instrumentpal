9e568687713e072e345eb77aed6c515af3cf57bf
Benjamin Hall <benjamin1995hall@gmail.com>
2023-03-15 13:33:06 +0000

    buttons & styling-reservations & instruments

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,6 +1,13 @@
 class InstrumentsController < ApplicationController
   def index
-    @instruments = Instrument.all
+    start_date = Date.today
+    end_date = Date.today + 1.month # or any other desired end_date
+    @instruments = available_instruments(start_date, end_date)
+  end
+
+  def available_instruments(start_date, end_date)
+    reserved_instruments = Reservation.where("start_date < ? AND end_date > ?", end_date, start_date).pluck(:instrument_id)
+    Instrument.where.not(id: reserved_instruments)
   end
 
   def show
