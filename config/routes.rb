cea87db9982373955122454a3f666224f9d24bb3
Benjamin Hall <benjamin1995hall@gmail.com>
2023-03-15 10:08:32 +0000

    added styling to reservations and added create method to reservations controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :instruments do
-    resources :reservations, only: [:new, :create]
+    resources :reservations, only: [:new, :create, :index, :show]
   end
 
   resources :reservations, only: [:index, :show, :edit, :update, :destroy]
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
