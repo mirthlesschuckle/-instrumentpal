a8183ab09e9c214a784059cbbb2a54ac2b345a5b
Jantinine <jantinederksen@gmail.com>
2023-03-17 11:09:46 +0100

    only owner can edit instruments

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,4 +1,8 @@
 class InstrumentsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_instrument, only: [:show, :edit, :update, :destroy]
+  before_action :check_owner, only: [:edit, :update, :destroy]
+
   def index
     start_date = Date.today
     end_date = Date.today + 1.month # or any other desired end_date
@@ -22,7 +26,7 @@ class InstrumentsController < ApplicationController
     @instrument = Instrument.new(instrument_params)
     @instrument.user = current_user
     @instrument.save
-    redirect_to instruments_path(@instrument)
+    redirect_to instruments_path(@instrument), alert: 'Instrument was successfully created.'
   end
 
   def edit
@@ -39,5 +43,15 @@ class InstrumentsController < ApplicationController
 
   def instrument_params
     params.require(:instrument).permit(:location, :model, :price, :photo)
+  end
+
+  def set_instrument
+    @instrument = Instrument.find(params[:id])
+  end
+
+  def check_owner
+    unless current_user == @instrument.user
+      redirect_to instruments_path, alert: "You can only edit instruments you own."
+    end
   end
 end
