c17703693b4ddebc4f8ae8055c4144805e52daf7
Benjamin Hall <benjamin1995hall@gmail.com>
2023-03-17 09:08:56 +0000

    reservations-tabs-styling-edit

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,13 +1,10 @@
 class ReservationsController < ApplicationController
   before_action :set_reservation, only: [:edit, :update]
+  before_action :set_reservation_for_update_status, only: [:update_status]
 
   def index
     @reservations = current_user.reservations
   end
-
-  # def show
-  #   @reservation = Reservation.find(params[:id])
-  # end
 
   def new
     @instrument = Instrument.find(params[:instrument_id])
@@ -36,13 +33,26 @@ class ReservationsController < ApplicationController
     end
   end
 
+  def update_status
+    if @reservation.update(status: params[:reservation][:status])
+      redirect_to reservations_path, notice: 'Reservation was successfully updated.'
+    else
+      render :edit, notice: 'Nothing has been changed.'
+    end
+  end
+
   private
 
   def set_reservation
     @reservation = current_user.reservations.find(params[:id])
   end
 
+  def set_reservation_for_update_status
+    instrument_ids = current_user.instruments.pluck(:id)
+    @reservation = Reservation.where(instrument_id: instrument_ids).find(params[:id])
+  end
+
   def reservation_params
-    params.require(:reservation).permit(:start_date, :end_date)
+    params.require(:reservation).permit(:start_date, :end_date, :status)
   end
 end
